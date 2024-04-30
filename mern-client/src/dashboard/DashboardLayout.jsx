2111174d1a2b85c93446068b731f286093784c54
Vinay Anand Lodhi <vinayanandlodhi12@gmail.com>
2024-04-30 23:07:48 +0530

    Your commit message here

---
+++ mern-client/src/dashboard/DashboardLayout.jsx
@@ -0,0 +1,17 @@
+
+import SideBar from './Sidebar';
+
+
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+
+function DashboardLayout() {
+  return (
+    <div className='flex gap-4 flex-col md:flex-row md:flex-row'>
+      <SideBar/>
+      <Outlet/>
+    </div>
+  )
+}
+
+export default DashboardLayout;
